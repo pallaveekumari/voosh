6aa78e1d7b5be183c73744efef3e1197e81afdb9
pallaveekumari <pallaveeratnakar1714@gmail.com>
2024-07-24 20:30:13 +0530

    redux-setup-material-ui-setup

--- frontend/src/Redux/store.js
+++ frontend/src/Redux/store.js
@@ -1,6 +1,6 @@
 import { reducer as AuthReducer } from "../Redux/AuthReducer/reducer";
 import { reducer as AppReducer } from "../Redux/AppReducer/reducer";
-import thunk from "redux-thunk";
+import {thunk} from "redux-thunk";
 import { legacy_createStore, applyMiddleware, combineReducers } from "redux";
 export const store = legacy_createStore(
   combineReducers({ AuthReducer, AppReducer }),
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { store } from './Redux/store';
 import {BrowserRouter} from "react-router-dom"
+import {ThemeProvider} from "@mui/material/styles"
+import theme from "./theme";
+import { store } from './Redux/store';
 import {Provider} from "react-redux"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
 <BrowserRouter>
+<ThemeProvider theme={theme}>
+
     <App />
+</ThemeProvider>
 </BrowserRouter>
   </Provider>
   
