90ef7d71b34c8529bb1e1a6fa880ed70e1d0d143
pallaveekumari <pallaveeratnakar1714@gmail.com>
2024-07-24 21:56:28 +0530

    navbar-implemented

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,8 +1,17 @@
+import { Box, Button } from '@mui/material'
 import React from 'react'
-
+import styles from "../Components/Navbar.module.css"
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+   <>
+   <Box className={styles.navbar}>
+  <Box className={styles.btnbox}>
+
+  <Button className={styles.loginbtn}>Login</Button>
+  <Button className={styles.signupbtn}>Signup</Button>
+  </Box>
+   </Box>
+   </>
   )
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom"
-import {ThemeProvider} from "@mui/material/styles"
-import theme from "./theme";
-import { store } from './Redux/store';
-import {Provider} from "react-redux"
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
+import theme  from "./theme";
+import { store } from "./Redux/store";
+import { Provider } from "react-redux";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-<BrowserRouter>
-<ThemeProvider theme={theme}>
-
-    <App />
-</ThemeProvider>
-</BrowserRouter>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </Provider>
-  
 );
 
 // If you want to start measuring performance in your app, pass a function
